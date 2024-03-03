d3d6b63cac1d58594df2cd2e5a93a94ea2fffc05
hi6nika <georgiew.m57@gmail.com>
2024-03-03 09:29:34 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   CONNECTION_STRING: "mongodb://localhost:27017",
-  DATA_BASE_NAME: "CarVilla",
+  DATA_BASE_NAME: "EzySell",
   PORT: 3000,
   SECRET: "BIGGESTSECRET",
   authTokenName: "X-Authorization",
